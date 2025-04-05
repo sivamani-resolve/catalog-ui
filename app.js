d7ed0b2a7a5c8ff3fa2a4943def9a4d10e160a09
Sivamani N <sivamani@rezolve.ai>
2025-04-05 15:28:11 +0530

    added the fifth feature

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ alert("This is a test alert from the app.js file in the catalog-ui directory.");
 alert("This is the third feature in the app.js file.");
 
 alert("This is the fourth feature in the app.js file.");
+
+alert("This is the fifth feature in the app.js file.");

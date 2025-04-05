deabe35f578d6a028fdef1be092deb4b27f73bc9
Sivamani N <sivamani@rezolve.ai>
2025-04-05 14:47:06 +0530

    3rd feature is implemented

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 alert("You are in the file: /C:/SivamaniN/Workouts/submodule-ui/catalog-ui/app.js");
 
 alert("This is a test alert from the app.js file in the catalog-ui directory.");
+
+alert("This is the third feature in the app.js file.");
